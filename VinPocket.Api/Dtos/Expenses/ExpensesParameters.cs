98db20447bc3dca72cb89b72b1a7875da5bd7095
subZero <marvinfordan08@gmail.com>
2026-01-31 15:54:11 +0800

    expenses - modify naming query string params naming conventions

--- VinPocket.Api/Dtos/Expenses/ExpensesParameters.cs
+++ VinPocket.Api/Dtos/Expenses/ExpensesParameters.cs
@@ -4,23 +4,31 @@ namespace VinPocket.Api.Dtos.Expenses;
 
 public sealed record ExpensesParameters
 {
-    [FromQuery]
+    [FromQuery(Name = "expense_date")]
     public DateOnly? ExpenseDate { get; init; }
-    [FromQuery]
+
+
+    [FromQuery(Name = "sort")]
     public string? Sort { get; init; }
-    [FromQuery]
+
+
+    [FromQuery(Name = "fields")]
     public string? Fields { get; init; }
-    [FromQuery]
-    public int Page { get; init; } = 1;
-    [FromQuery]
-    public int PageSize { get; init; } = 10;
+
+
+    [FromQuery(Name = "page")]
+    public int Page { get; set; } = 1;
+
+
+    [FromQuery(Name = "page_size")]
+    public int PageSize { get; set; } = 10;
 
     public void Deconstruct(
-       out DateOnly? expenseDate,
-       out string? sort,
-       out string? fields,
-       out int page,
-       out int pageSize)
+        out DateOnly? expenseDate,
+        out string? sort,
+        out string? fields,
+        out int page,
+        out int pageSize)
     {
         expenseDate = ExpenseDate;
         sort = Sort;
